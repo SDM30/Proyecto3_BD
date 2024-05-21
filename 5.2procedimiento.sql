16006942a008226fe0f8e46cd875520ad7b74cde
Simon <151746540+SDM30@users.noreply.github.com>
2024-05-20 21:17:19 -0500

    Create 5.2procedimiento.sql

---
+++ 5.2procedimiento.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE actualizar_precio_productoTotal(
+    p_CodigoPedido  VARCHAR2
+) AS
+    precio NUMBER;
+BEGIN
+    SELECT SUM (CANTIDAD*PRECIOUNIDAD)
+    INTO PRECIO
+    FROM DETALLEPEDIDOS
+    WHERE CODIGOPEDIDO = P_CODIGOPEDIDO;
+    
+    UPDATE PEDIDOS
+    SET VALORTOTAL = PRECIO
+    WHERE CODIGOPEDIDO = p_codigoPedido;
+    
+    COMMIT;
+END actualizar_precio_productoTotal;
+/
+
+BEGIN
+    actualizar_precio_productoTotal(1);
+END;
+/
