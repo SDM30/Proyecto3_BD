91f00d642c3ac7f5f7a643bca9075e9a1a98f15d
Simon <151746540+SDM30@users.noreply.github.com>
2024-05-20 21:06:28 -0500

    Create 3.1disparador.sql

---
+++ 3.1disparador.sql
@@ -0,0 +1,11 @@
+ALTER TRIGGER productos_actualizar_stock COMPILE;
+create or replace trigger productos_actualizar_stock
+after insert on detallepedidos FOR EACH ROW
+declare
+begin
+UPDATE productos
+    SET cantidadenstock = cantidadenstock - :new.cantidad
+    where codigoproducto = :new.codigoproducto;
+end;
+
+
